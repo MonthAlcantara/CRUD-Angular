fceda1bf1b9e540ef80763eb9386a164faee9597
Montival Junior <montival_junior@yahoo.com.br>
2020-07-16 11:11:33 -0300

    Adicionado Footer ao projeto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
