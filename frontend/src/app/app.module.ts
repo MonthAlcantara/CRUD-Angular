f1b35028f30ff5358e868ccfed66f2707686c7a8
Montival Junior <montival_junior@yahoo.com.br>
2020-07-16 11:42:09 -0300

    Criado componente de navegação

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,10 +8,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
