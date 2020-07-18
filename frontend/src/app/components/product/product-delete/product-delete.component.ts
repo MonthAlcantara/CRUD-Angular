85c5af2fdce3ac10b82713d46fd60c971c5bb98d
Montival Junior <montival_junior@yahoo.com.br>
2020-07-18 16:50:54 -0300

    Adicionado componente e lógica para delete

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,37 @@
+import { ProductService } from './../product.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css'],
+})
+export class ProductDeleteComponent implements OnInit {
+  product: Product;
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.readById(id).subscribe((product) => {
+      this.product = product;
+    });
+  }
+
+  deleteProduct(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.delete(id).subscribe(() => {
+      this.productService.showMessage('Produto excluído com Sucesso!');
+    });
+    this.router.navigate(['/products']);
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
+}
