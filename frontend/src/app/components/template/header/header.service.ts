e8d9b034ac0153401d1133436e3a72e9c1bd7f16
Montival Junior <montival_junior@yahoo.com.br>
2020-07-18 17:31:08 -0300

    Adicionado alteração de title ao mudar de pagina

--- frontend/src/app/components/template/header/header.service.ts
+++ frontend/src/app/components/template/header/header.service.ts
@@ -1,9 +1,23 @@
+import { HeaderData } from './header-data.module';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeaderService {
+  private _headerData = new BehaviorSubject<HeaderData>({
+    title: 'Inicio',
+    icon: 'home',
+    routeUrl: '',
+  });
 
-  constructor() { }
+  constructor() {}
+
+  get headerData(): HeaderData {
+    return this._headerData.value;
+  }
+  set headerData(headerData: HeaderData) {
+    this._headerData.next(headerData);
+  }
 }
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,7 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./product-crud.component.css'],
 })
 export class ProductCrudComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products',
+    };
+  }
 
   ngOnInit(): void {}
 
